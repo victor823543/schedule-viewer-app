cf86288fcb044a67b323b242d10cea5f263515a1
Victor <v.safsten@gmail.com>
2024-12-10 18:00:15 +0100

    Fix animation issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideAnimationsAsync()]
+}).catch((err) => console.error(err));
