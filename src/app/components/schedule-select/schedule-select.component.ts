ff3f935d6aa48bc5b4d1d58a81ea0b1b79a9f5c8
Victor <v.safsten@gmail.com>
2024-12-12 13:29:41 +0100

    Style toolbar and change to custom select

--- src/app/components/schedule-select/schedule-select.component.ts
+++ src/app/components/schedule-select/schedule-select.component.ts
@@ -1,7 +1,10 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { Observable } from 'rxjs';
@@ -10,14 +13,48 @@ import { SchedulesService } from '../../services/schedules.service';
 
 @Component({
   selector: 'app-schedule-select',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, CommonModule],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './schedule-select.component.html',
-  styleUrl: './schedule-select.component.scss'
+  styleUrl: './schedule-select.component.scss',
+  animations: [
+    trigger('dropdownAnimation', [
+      state(
+        'closed',
+        style({
+          opacity: 0,
+          transform: 'scaleY(0)',
+          transformOrigin: 'top'
+        })
+      ),
+      state(
+        'open',
+        style({
+          opacity: 1,
+          transform: 'scaleY(1)',
+          transformOrigin: 'top'
+        })
+      ),
+      transition('closed <=> open', [animate('200ms ease-in-out')])
+    ])
+  ]
 })
 export class ScheduleSelectComponent implements OnInit {
   schedules$!: Observable<SchedulesResponse>;
 
   selectedSchedule: Entity | null = null;
+
+  dropdownOpen = false;
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 
   constructor(private schedulesService: SchedulesService) {}
 
@@ -27,11 +64,10 @@ export class ScheduleSelectComponent implements OnInit {
     this.schedulesService.selectedSchedule$.subscribe((schedule) => {
       this.selectedSchedule = schedule;
     });
-    console.log('Selected schedule:', this.selectedSchedule);
   }
 
   onScheduleSelect(scheduleId: string): void {
-    console.log('Selected schedule:', scheduleId);
+    this.dropdownOpen = false;
     this.schedules$.subscribe((schedules) => {
       const selected = schedules.find((schedule) => schedule.id === scheduleId) || null;
       this.schedulesService.setSelectedSchedule(selected);
