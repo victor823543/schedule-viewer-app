4ded397501103ff574982f3752ced3a6b8bbb7bd
Victor <v.safsten@gmail.com>
2024-12-15 20:28:03 +0100

    Make logo into link back to homepage

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { CreateEntitySectionComponent } from '../../components/create-entity-section/create-entity-section.component';
 import { EntitySectionComponent } from '../../components/entity-section/entity-section.component';
 import { ScheduleSelectComponent } from '../../components/schedule-select/schedule-select.component';
@@ -10,7 +11,8 @@ import { ScheduleSelectComponent } from '../../components/schedule-select/schedu
     ScheduleSelectComponent,
     MatToolbarModule,
     EntitySectionComponent,
-    CreateEntitySectionComponent
+    CreateEntitySectionComponent,
+    RouterModule
   ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
