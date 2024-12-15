7e6b0d7dcc9a882c6f615f700de6e689d4774eb2
Victor <v.safsten@gmail.com>
2024-12-15 21:04:43 +0100

    Delete select schedule dialog

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit, signal } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { combineLatest, filter, map, Subject, take, takeUntil } from 'rxjs';
+import { map, Subject, takeUntil } from 'rxjs';
 import { CalendarComponent } from '../../components/calendar/calendar.component';
 import { DatePickerComponent } from '../../components/date-picker/date-picker.component';
-import { DialogComponent } from '../../components/dialog/dialog.component';
 import { FilterChipsComponent } from '../../components/filter-chips/filter-chips.component';
 import { FilterComponent } from '../../components/filter/filter.component';
 import { ScheduleSelectComponent } from '../../components/schedule-select/schedule-select.component';
 import { CalendarEvent } from '../../models/calendar.model';
 import { CalendarService } from '../../services/calendar.service';
 import { FilterService } from '../../services/filter.service';
-import { SchedulesService } from '../../services/schedules.service';
 
 @Component({
   selector: 'app-home',
@@ -51,14 +48,10 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   constructor(
     private calendarService: CalendarService,
-    private schedulesService: SchedulesService,
-    private filterService: FilterService,
-    private dialog: MatDialog
+    private filterService: FilterService
   ) {}
 
   ngOnInit(): void {
-    this.checkAndOpenDialog();
-
     this.calendarService.calendarEvents$
       .pipe(
         takeUntil(this.destroy$),
@@ -81,33 +74,5 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   setView(view: 'day' | 'week'): void {
     this.selectedView.set(view);
-  }
-
-  private checkAndOpenDialog(): void {
-    // this.schedulesService.ensureSchedulesLoaded();
-
-    combineLatest([this.schedulesService.selectedSchedule$, this.schedulesService.schedules$])
-      .pipe(
-        filter(
-          ([selectedSchedule, schedules]) => selectedSchedule === null && schedules.length > 0
-        ),
-        take(1)
-      )
-      .subscribe(([_, schedules]) => {
-        const dialogRef = this.dialog.open(DialogComponent, {
-          data: { schedules },
-          disableClose: true,
-          minHeight: '300px',
-          minWidth: '300px'
-        });
-
-        dialogRef.afterClosed().subscribe((selectedItem) => {
-          if (selectedItem) {
-            this.schedulesService.setSelectedSchedule(selectedItem);
-          } else {
-            console.warn('No item selected!');
-          }
-        });
-      });
   }
 }
